a8edc6dae01449af529c17b45b536b11cc2a9e85
Alex <alexanderwbw@gmail.com>
2023-04-19 12:00:28 +0700

    Biodata

---
+++ src/Biodata.java
@@ -0,0 +1,13 @@
+public class Biodata {
+    public static void main(String[] args) {
+        String a = "22410100047";
+        String b = "Alexander Wibowo";
+        String c = "19 tahun";
+        String d = "Rungkut";
+        String e = "Badminton";
+        String f = "Single";
+        String g = "Surabaya, 15 Januari 2004";
+
+        System.out.println("BIODATA" + "\nNIM \t: " + a + "\nNama \t: " + b + "\nUsia \t: " + c + "\nAlamat \t: " + d + "\nHobi \t: " + e + "\nStatus \t: " + f + "\nTempat, tanggal lahir \t: " + g);
+    }
+}
